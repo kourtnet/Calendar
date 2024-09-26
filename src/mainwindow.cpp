6ca544bf28209ee2bf02907921d4c35279aff86d
Kourtnet <itromart@gmail.com>
2024-09-26 23:15:12 +0300

    master | a bit of testing

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,16 +1,15 @@
-
 #include <iostream>
-#include "calendarTask.h"
 #include "calendarEvent.h"
 
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
-	CalendarEvent ev = CalendarEvent(QString("nastyas birthday", "nastyas birthday in hinklanaya", QDateTime::fromString("2025-01-04 00:00:00"), QDateTime::fromString("2025-01-04 23:59:59"));
-//CalendarTask tsk = CalendarEvent(QString("homework", "math homework", QDateTime::fromString("2025-02-23 23:00:00")));
+	QString time_format = "yyyy-MM-dd HH:mm:ss";
+	CalendarEvent ev = CalendarEvent(QString("nastyas birthday"), QString("nastyas birthday in hinklanaya"), QDateTime::fromString("2025-01-04 00:00:00", time_format, QCalendar(QCalendar::System::Gregorian)), QDateTime::fromString("2025-01-04 23:59:59", time_format, QCalendar(QCalendar::System::Gregorian)));
+CalendarTask tsk = CalendarTask(QString("homework"), QString("math homework"), QDateTime::fromString("2025-02-23 23:00:00", time_format, QCalendar(QCalendar::System::Gregorian)));
 
-//	std::cout<<"event\nname: "<<ev.get_name() << "\ndesrc: "<< ev.get_description()<<"\nbegining: ";//<<ev.get_date_time_begin()<< "\nend: "<<ev.get_date_time_end();
-	//std::cout<<"task\nname: "<<tsk.get_name() << "\ndesrc: "<< tsk.get_description()<<"\ndeadline: "<<tsk.get_date_time_end();
+	std::cout<<"event\nname: "<<ev.get_name().toLocal8Bit().data() << "\ndesrc: "<< ev.get_description().toLocal8Bit().data()<<"\nbegining: "<<ev.get_date_time_begin().toString(time_format, QCalendar(QCalendar::System::Gregorian)).toLocal8Bit().data()<< "\nend: "<<ev.get_date_time_end().toString(time_format, QCalendar(QCalendar::System::Gregorian)).toLocal8Bit().data();
+	std::cout<<"\ntask\nname: "<<tsk.get_name().toLocal8Bit().data()<< "\ndesrc: "<< tsk.get_description().toLocal8Bit().data()<<"\ndeadline: "<<tsk.get_date_time_end().toString(time_format, QCalendar(QCalendar::System::Gregorian)).toLocal8Bit().data();
 
 }
 MainWindow::~MainWindow() {
