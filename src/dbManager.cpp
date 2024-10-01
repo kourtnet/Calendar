a31f1f08fc29b74ca1eb5e6992dcb6212378f41a
Kourtnet mac <itromart@gmail.com>
2024-10-01 17:00:26 +0300

    develop | defined insert methods for db manager

--- src/dbManager.cpp
+++ src/dbManager.cpp
@@ -3,6 +3,7 @@
 DbManager::DbManager(QString inputPath){
 	path = inputPath;
 	db = QSqlDatabase::addDatabase("QSQLITE"); 
+	db.setDatabaseName(path);
 }
 
 DbManager::~DbManager() {}
@@ -14,9 +15,7 @@ bool DbManager::db_exists() {
 
 //returns "Success" if db opened successfully and some error text if db didnt open for some reason
 QString DbManager::db_create() {
-	//assign path from db manager object
-	db.setDatabaseName(path);
-	QString res = QString("Success");
+	QString res = QString(SUCCESS_QUERY_MESSAGE);
 	//send error if couldnt open for some reason, else create tables
 	if (!db.open()) {
 		res = db.lastError().text();	
@@ -26,12 +25,66 @@ QString DbManager::db_create() {
 		createTablesQuery.exec(CREATE_TASKS_TABLE_QUERY);
 		//create events table
 		bool ls = createTablesQuery.exec(CREATE_EVENTS_TABLE_QUERY);
-		//if ls is false, that mean that tables are not created, send error
+		//if ls is false, that means that tables are not created, send error
 		if (!ls) res = createTablesQuery.lastError().text();
 	}
 	return res;
 }
 
+
+QString DbManager::db_insert_tasks(QString name, QString description, QDateTime deadline) {
+	QString res = QString(SUCCESS_QUERY_MESSAGE);
+	//send error if couldnt open for some reason, else insert into table
+	if (!db.open()) {
+		res = db.lastError().text();
+	} else {
+		QSqlQuery insertTasksTableQuery = QSqlQuery(db);
+		insertTasksTableQuery.prepare(INSERT_TASKS_TABLE_QUERY);
+		//0 is name column
+		insertTasksTableQuery.bindValue(0, name);
+		//1 is description column
+		insertTasksTableQuery.bindValue(1, description);
+		//2 is deadline column
+		insertTasksTableQuery.bindValue(2, deadline.toString("yyyy-MM-dd hh:mm:ss"));
+		//exec after binding parameters
+		bool ls = insertTasksTableQuery.exec();
+		//if ls is false, that means that values were not inserted, send error
+		if(!ls) res = insertTasksTableQuery.lastError().text();
+	}
+	return res;
+}
+
+QString DbManager::db_insert_events(QString name, QString description, QDateTime date_time_begin, QDateTime date_time_end) {
+	QString res = QString(SUCCESS_QUERY_MESSAGE);
+	//send error if couldnt open for some reason, else insert into table
+	if (!db.open()) {
+		res = db.lastError().text();
+	} else {
+		QSqlQuery insertEventsTableQuery = QSqlQuery(db);
+		insertEventsTableQuery.prepare(INSERT_EVENTS_TABLE_QUERY);
+		//0 is name column
+		insertEventsTableQuery.bindValue(0, name);
+		//1 is description column
+		insertEventsTableQuery.bindValue(1, description);
+		//2 is date_time_begin column
+		insertEventsTableQuery.bindValue(2, date_time_begin.toString("yyyy-MM-dd hh:mm:ss"));
+		//3 is date_time_begin column
+		insertEventsTableQuery.bindValue(3, date_time_end.toString("yyyy-MM-dd hh:mm:ss"));
+		//exec after binding parameters
+		bool ls = insertEventsTableQuery.exec();
+		//if ls is false, that means that values were not inserted, send error
+		if(!ls) res = insertEventsTableQuery.lastError().text();
+	}
+	return res;
+}
+/*QString DbManager::db_insert_events() {
+
+}
+
+QString DbManager::db_insert_events(QString name, QString description, QDateTime) {
+
+}
+*/
 QString DbManager::get_path(){
 	return path;
 }
--- src/dbManager.h
+++ src/dbManager.h
@@ -1,8 +1,14 @@
 #ifndef DB_MANAGER_H
 #define DB_MANAGER_H
 
-#define CREATE_EVENTS_TABLE_QUERY "CREATE TABLE tasks(id INTEGER PRIMARY KEY, name TEXT, description TEXT, date_time_end DATETIME);"
-#define CREATE_TASKS_TABLE_QUERY "CREATE TABLE events(id INTEGER PRIMARY KEY, name TEXT, description TEXT, date_time_begin DATETIME, date_time_end DATETIME);"
+#define SUCCESS_QUERY_MESSAGE "SUCCESS"
+
+#define CREATE_TASKS_TABLE_QUERY "CREATE TABLE tasks(id INTEGER PRIMARY KEY, name TEXT, description TEXT, deadline DATETIME);"
+#define CREATE_EVENTS_TABLE_QUERY "CREATE TABLE events(id INTEGER PRIMARY KEY, name TEXT, description TEXT, date_time_begin DATETIME, date_time_end DATETIME);"
+
+#define INSERT_TASKS_TABLE_QUERY "INSERT INTO tasks(name, description, deadline) VALUES(?, ?, datetime(?))"
+#define INSERT_EVENTS_TABLE_QUERY "INSERT INTO events(name, description, date_time_begin, date_time_end) VALUES(?, ?, datetime(?))"
+
 #include <QtSql>
 #include <QFile>
 class DbManager {
@@ -11,7 +17,8 @@ class DbManager {
 		~DbManager();
 		bool db_exists();
 		QString db_create();
-		QString db_insert(int table, QString name, QString description, QDateTime timeEnd, QDateTime timeBegin = null);
+		QString db_insert_tasks(QString name, QString description, QDateTime deadline);
+		QString db_insert_events(QString name, QString description, QDateTime timeBegin, QDateTime timeEnd);
 		QString db_delete(/*?????*/);
 		QString db_alter(/*??????*/);
 		QString get_path(/*?????*/);
